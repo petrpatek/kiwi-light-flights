75e13180e818dfdc4c3b90bb4b1c0d1ecf2964cf
Petr Patek <petr.patek@plus4u.net>
2018-02-21 16:47:22 +0100

    improved date handling in request helper

--- src/request-handler/request-handler.js
+++ src/request-handler/request-handler.js
@@ -1,10 +1,18 @@
-import axios from "axios"
+import axios from "axios";
+import moment from "moment"
 
 class RequestHandler {
-
-    static async getFlights(from,to,dateFrom,dateTo){
-       let flights= await  axios.get(`https://api.skypicker.com/flights?flyFrom=${from}&to=${to}&dateFrom=${dateFrom}&dateTo=${dateTo}`)
-        return flights
-
-    }
+  static _formatDate(date){
+    return moment(date).format("DD/MM/YYYY")
+  }
+  static  getFlights(from, to, dateFrom, dateTo) {
+    return axios.get(
+      `https://api.skypicker.com/flights?flyFrom=${encodeURIComponent(
+        from
+      )}&to=${encodeURIComponent(to)}&dateFrom=${encodeURIComponent(
+          this._formatDate(dateFrom)
+      )}&dateTo=${encodeURIComponent(this._formatDate(dateTo))}&partner=picky&partner_market=us`
+    );
+  }
 }
+export default RequestHandler;
