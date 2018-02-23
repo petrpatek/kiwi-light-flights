1b38c2f2b3d477e707c55b237498a13f5dc936bb
Petr Patek <petr.patek@plus4u.net>
2018-02-23 08:57:08 +0100

    added offset to request handler flights call and installed react paginate

--- src/request-handler/request-handler.js
+++ src/request-handler/request-handler.js
@@ -5,13 +5,14 @@ class RequestHandler {
   static _formatDate(date){
     return moment(date).format("DD/MM/YYYY")
   }
-  static  getFlights(from, to, dateFrom, dateTo) {
+  static  getFlights(from, to, dateFrom, dateTo, offset) {
+      offset = offset ? offset : 0;
     return axios.get(
       `https://api.skypicker.com/flights?flyFrom=${encodeURIComponent(
         from
       )}&to=${encodeURIComponent(to)}&dateFrom=${encodeURIComponent(
           this._formatDate(dateFrom)
-      )}&dateTo=${encodeURIComponent(this._formatDate(dateTo))}&partner=picky&partner_market=us`
+      )}&dateTo=${encodeURIComponent(this._formatDate(dateTo))}&partner=picky&partner_market=us&offset=${offset}`
     );
   }
   static getLocations(term) {
